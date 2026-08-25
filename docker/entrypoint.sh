2b97d3f748a9b213483568fa34dfa88118575dd8
miteshparadox <ravimandal.here@gmail.com>
2026-08-26 01:52:35 +0545

    Fix Railway Apache port startup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,8 @@
 set -eu
 
 port="${PORT:-8080}"
-sed -ri "s/^Listen [0-9]+/Listen ${port}/" /etc/apache2/ports.conf
-sed -ri "s/<VirtualHost \*:([0-9]+)>/<VirtualHost *:${port}>/" /etc/apache2/sites-available/000-default.conf
 
-exec apache2-foreground
+sed -ri "s/^Listen [0-9]+/Listen ${port}/" /etc/apache2/ports.conf
+sed -ri "s/<VirtualHost \*:[0-9]+>/<VirtualHost *:${port}>/" /etc/apache2/sites-available/000-default.conf
+
+exec apache2-foreground "$@"
